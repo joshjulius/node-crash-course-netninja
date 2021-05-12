1902523aeb4e7ca3d7dcd54a7e51067859459ae9
Joshua Julius <joshjulius1@gmail.com>
2021-05-11 19:17:55 -0500

    finish course

--- app.js
+++ app.js
@@ -1,27 +1,91 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb
+const dbURI = 'mongodb+srv://joshjulius:josh@mern-shopping.z08ak.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('connected to db');
+        // listen for requests
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
 
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware and static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//     next();
+// });
+
+// // mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'new blog 2',
+//         snippet: 'about my new blog',
+//         body: 'more about my new blog'
+//     });
+
+//     blog.save()
+//         .then(result => res.send(result))
+//         .catch(err => console.log(err));
+// });
+
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//         .then(result => res.send(result))
+//         .catch(err => console.log(err));
+// });
+
+// app.get('/single-blog', (req, res) => {
+//    Blog.findById("609b02ab1c6f2454fc722472")
+//     .then(result => res.send(result))
+//     .catch(err => console.log(err));
+// });
+
+// routes
 app.get('/', (req, res) => {
     //res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', {title: 'About'});
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// // redirects
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // 404 page (need to be written last), 404 status needs to be manually set
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    // res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', {title: '404'});
 });
